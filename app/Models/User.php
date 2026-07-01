c0d6ff3e4635993005632ac7e8effa50d2be38bc
Jyoti Pandey <pandeyjyoti1110@email.com>
2026-07-01 13:16:34 +0530

    added email notification

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,10 +33,7 @@ class User extends Authenticatable
     return [
         'email_verified_at'      => 'datetime',
         'password'               => 'hashed',
-        'notify_ticket_created'  => 'boolean',
-        'notify_ticket_assigned' => 'boolean',
-        'notify_status_changed'  => 'boolean',
-        'notify_comment_added'   => 'boolean',
+        'email_notification' => 'array',
     ];
 }
 
